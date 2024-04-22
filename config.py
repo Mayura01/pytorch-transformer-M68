d9902e52809038e44feb3a7160b4f98b5dc99550
Mayur <mayurpatkar68@gmail.com>
2024-04-22 13:59:02 +0530

    changing two to single tokenizer

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 4,
+        "batch_size": 8,
         "num_epochs": 100,
         "lr": 10**-4,
         "seq_len": 400,
         "d_model": 512,
         "model_file_path": "model_M68/M68.pt",
-        "tokenizer_file": "dataset_and_tokenizers/tokenizer_{0}.json",
+        "tokenizer": "dataset_and_tokenizer/tokenizer.json",
     }
 
 def get_weights_file_path(config):
